3217ae50aba8fcdcd6964de8d5cf1e1799cd5815
Rem M. <morrig407@gmail.com>
2025-03-06 22:48:55 +0700

    Add model Book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    author = models.CharField(max_length=255, verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        ordering = ['-name', '-author']
+

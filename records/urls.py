6f92fa9ec3e0b06e842a3471b155fc9e10480ecd
Rem M. <morrig407@gmail.com>
2025-03-08 19:15:25 +0700

    Add record updation view

--- records/urls.py
+++ records/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import RecordListView, RecordCreateView
+from .views import RecordListView, RecordCreateView, RecordUpdateView
 
 
 urlpatterns = [
     path('', RecordListView.as_view(), name='record_list'),
     path('create/', RecordCreateView.as_view(), name='record_create'),
+    path('<int:record_id>/update/', RecordUpdateView.as_view(),
+         name='record_update'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Record
@@ -16,3 +16,11 @@ class RecordCreateView(CreateView):
     form_class = RecordForm
     success_url = reverse_lazy('record_list')
     template_name = 'records/form.html'
+
+
+class RecordUpdateView(UpdateView):
+    model = Record
+    form_class = RecordForm
+    success_url = reverse_lazy('record_list')
+    template_name = 'records/form.html'
+    pk_url_kwarg = 'record_id'

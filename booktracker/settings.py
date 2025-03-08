f01155d36ad2f0c8699aaee1312e0a8b1ac7a458
Rem M. <morrig407@gmail.com>
2025-03-08 11:47:09 +0700

    Moves templates/ to the root; add templates/ to settings

--- booktracker/settings.py
+++ booktracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'booktracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

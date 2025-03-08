ff69f827558356e29301b396452c905a29df8edc
Rem M. <morrig407@gmail.com>
2025-03-08 14:11:16 +0700

    Change form instance for Book to ModelForm

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+from .models import Book
 
-class BookForm(forms.Form):
-    name = forms.CharField(max_length=255, label='Название')
-    author = forms.CharField(max_length=255, label='Автор')
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('name', 'author',)

0cde58b3e13c4804da18efc6913ce74e1f31af1f
Rem M. <morrig407@gmail.com>
2025-03-08 11:41:08 +0700

    Add books list

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexPageView
+from .views import IndexPageView, BooksView
 
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index_page'),
+    path('list/', BooksView.as_view(), name='books_list'),
 ]
--- books/views.py
+++ books/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+from .models import Book
+
 
 class IndexPageView(View):
     def get(self, request):
         return HttpResponse('<h1>Hello, world!</h1>')
+
+
+class BooksView(View):
+    def get(self, request):
+        books = Book.objects.all()
+        return render(request, 'books/list.html', {'books': books})

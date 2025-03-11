ac4d5924a3fc926b7dcc47cb62f4fc32204313f1
Rem M. <morrig407@gmail.com>
2025-03-11 20:45:15 +0700

    Add detail view for Record

--- records/views.py
+++ records/views.py
@@ -1,9 +1,17 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Record
 from .forms import RecordForm
+
+
+class RecordDetailView(DetailView):
+    model = Record
+    template_name = 'records/detail.html'
+    context_object_name = 'record'
+    pk_url_kwarg = 'record_id'
 
 
 class RecordListView(ListView):

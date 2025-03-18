5ce4fd786ac47919ac8f1757dfec23432297be4e
Rem M. <morrig407@gmail.com>
2025-03-18 21:50:25 +0700

    Add model Shelf

--- shelves/models.py
+++ shelves/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from books.models import Book
+
+
+class Shelf(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    book = models.ManyToManyField(Book,
+                                  related_name='books',
+                                  verbose_name='Книги')
+    user = models.ForeignKey(User, verbose_name='Пользователь',
+                             on_delete=models.PROTECT)

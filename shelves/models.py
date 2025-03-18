9f85922f12e5ca8aef53637be70dba93b1c3e76b
Rem M. <morrig407@gmail.com>
2025-03-18 21:57:14 +0700

    Model Sheld changes: rename book to books, add verbose names

--- shelves/models.py
+++ shelves/models.py
@@ -6,8 +6,15 @@ from books.models import Book
 
 class Shelf(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
-    book = models.ManyToManyField(Book,
-                                  related_name='books',
-                                  verbose_name='Книги')
     user = models.ForeignKey(User, verbose_name='Пользователь',
                              on_delete=models.PROTECT)
+    books = models.ManyToManyField(Book,
+                                   related_name='books',
+                                   verbose_name='Книги')
+
+    class Meta:
+        verbose_name = 'Полка'
+        verbose_name_plural = 'Полки'
+
+    def __str__(self):
+        return f"{self.name} ({self.user.username})"

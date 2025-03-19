584077d31d97b50de6c604ef7b943d4a51305606
Rem M. <morrig407@gmail.com>
2025-03-19 21:15:07 +0700

    Add Get handler for shelf creation

--- shelves/urls.py
+++ shelves/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import shelf_list_view, shelf_detail_view
+from .views import shelf_list_view, shelf_detail_view, shelf_create_view
 
 
 urlpatterns = [
     path('', shelf_list_view, name='shelf_list'),
+    path('create/', shelf_create_view, name='shelf_create'),
     path('<int:shelf_id>/', shelf_detail_view, name='shelf_detail'),
 ]
--- shelves/views.py
+++ shelves/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
+from .forms import ShelfForm
 from .models import Shelf
 
 
@@ -13,3 +14,9 @@ def shelf_detail_view(request, shelf_id):
     if request.method == 'GET':
         shelf = get_object_or_404(Shelf, id=shelf_id)
         return render(request, 'shelves/detail.html', {'shelf': shelf})
+
+
+def shelf_create_view(request):
+    if request.method == 'GET':
+        form = ShelfForm()
+        return render(request, 'shelves/form.html', {'form': form})
